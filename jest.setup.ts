a21da9cf731c24c7165ceadf0b3c8e63a30d743e
Matheusafonsouza <matheusafonsouza@gmail.com>
2024-12-08 17:22:54 -0300

    fix: tests for app
    Co-Authored-By: Joaovitor045 <joaovitoralvestf@gmail.com>
    Co-Authored-By: gsVieiraaa <Gabriel.vieira.santos.110@gmail.com>

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+
+if (
+  typeof globalThis.TextEncoder === "undefined" ||
+  typeof globalThis.TextDecoder === "undefined"
+) {
+  const utils = require("util");
+  globalThis.TextEncoder = utils.TextEncoder;
+  globalThis.TextDecoder = utils.TextDecoder;
+  globalThis.Uint8Array = Uint8Array;
+}
+
+global.structuredClone = (val) => JSON.parse(JSON.stringify(val))
