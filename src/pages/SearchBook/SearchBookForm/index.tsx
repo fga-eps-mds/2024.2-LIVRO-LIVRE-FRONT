f6d486653920b691f80c850d85e2a792b4cd9a3a
Guilherme Zanella <guilhermeczanella@gmail.com>
2025-01-20 18:02:16 -0300

    feat:Att searchBook Page Co-authored-by: Bruno Cruz <bruno.g.rosa@hotmail.com.br>

--- src/pages/SearchBook/SearchBookForm/index.tsx
+++ src/pages/SearchBook/SearchBookForm/index.tsx
@@ -1,17 +1,16 @@
-import { Input, Stack, Separator, Center } from '@chakra-ui/react';
+import { Input, Center, Box } from '@chakra-ui/react';
 import { IoSearchOutline } from "react-icons/io5";
 import { InputGroup } from '../../../components/ui/input-group';
 
 function SearchBookForm() {
     return (
-        <Stack gap={'5px'} align-items={'center'} justifyContent={'center'}>
-            <Separator />
+        <Box gap={'5px'} mt={'15px'} >
            <Center>
-           <InputGroup flex="0.9" startElement={<IoSearchOutline />}>
-             <Input type="text" placeholder="Digite o nome, autor ou tema do livro que deseja." />
+           <InputGroup flex="0.85" startElement={<IoSearchOutline />}>
+             <Input  type="text" placeholder="Digite o nome, autor ou tema do livro desejado." />
            </InputGroup>
            </Center>
-        </Stack>
+        </Box>
     );
 }
 
--- src/pages/SearchBook/SearchBookGrid/index.tsx
+++ src/pages/SearchBook/SearchBookGrid/index.tsx
@@ -28,6 +28,26 @@ function SearchBookGrid() {
                 <Text textStyle={'sm'}>Autor do Livro</Text>
                 <Rating  readOnly allowHalf defaultValue={4.5} size={'xs'} colorPalette={'yellow'} />
             </Container>
+            <Container  width={'150px'} bg={'gray.100'} p={'10px'} shadow={"rgba(0, 0, 0, 0.16) 0px 1px 4px;"}
+             borderRadius={'4px'} justifySelf={'center'} height={'260px'}> 
+                <Image
+                    src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="Capa do Livro"
+                />
+                <Heading size={'lg'} mt={'2'}>Título do Livro</Heading>
+                <Text textStyle={'sm'}>Autor do Livro</Text>
+                <Rating  readOnly allowHalf defaultValue={4.5} size={'xs'} colorPalette={'yellow'} />
+            </Container>
+            <Container  width={'150px'} bg={'gray.100'} p={'10px'} shadow={"rgba(0, 0, 0, 0.16) 0px 1px 4px;"}
+             borderRadius={'4px'} justifySelf={'center'} height={'260px'}> 
+                <Image
+                    src="https://plus.unsplash.com/premium_photo-1682125773446-259ce64f9dd7?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="Capa do Livro"
+                />
+                <Heading size={'lg'} mt={'2'}>Título do Livro</Heading>
+                <Text textStyle={'sm'}>Autor do Livro</Text>
+                <Rating  readOnly allowHalf defaultValue={4.5} size={'xs'} colorPalette={'yellow'} />
+            </Container>
             </Grid>
         </Stack>
     );
